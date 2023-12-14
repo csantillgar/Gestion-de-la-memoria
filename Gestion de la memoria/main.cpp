0b94fee1e7367d368b57c750331e31feb06c8361
csantillgar <csantill@myuax.com>
2023-12-14 11:08:04 +0100

    gestion de la memoria

--- Gestion de la memoria/main.cpp
+++ Gestion de la memoria/main.cpp
@@ -7,6 +7,7 @@
 int main() {
     HANDLE hMapFile;
     LPCTSTR pBuf;
+
     hMapFile = CreateFileMapping(
             INVALID_HANDLE_VALUE,   // Handle del archivo mapeado
             NULL,                   // Atributos de seguridad
@@ -14,38 +15,41 @@ int main() {
             0,                      // Tamaño máximo de la memoria (alto)
             SIZE,                   // Tamaño máximo de la memoria (bajo)
             L"MemoriaCompartida");  // Nombre del archivo mapeado
+
     if (hMapFile == NULL) {
         perror("CreateFileMapping");
         return 1;
     }
+
     pBuf = (LPTSTR)MapViewOfFile(
             hMapFile,               // Handle del archivo mapeado
             FILE_MAP_ALL_ACCESS,    // Permisos de acceso
             0,
             0,
             SIZE);
+
     if (pBuf == NULL) {
         perror("MapViewOfFile");
         CloseHandle(hMapFile);
         return 1;
-
     }
+
     pid_t pid = fork();
 
     if (pid < 0) {
         perror("fork");
         return 1;
     } else if (pid == 0) {
-        printf("Child reads: %s\n",pBuf);
-        munmap(shared_memory, SIZE);
+        printf("Child reads: %s\n", pBuf);
         UnmapViewOfFile(pBuf);
         CloseHandle(hMapFile);
-        exit(EXIT_SUCCESS);
         return 0;
     } else {
-        strcpy(shared_memory, "Hello, child process!");
-        wait(NULL);
-        munmap(shared_memory, SIZE);
+        strcpy(pBuf, "Hello, child process!");
+        WaitForSingleObject(pid, INFINITE);
+        UnmapViewOfFile(pBuf);
+        CloseHandle(hMapFile);
+        return 0;
     }
 
     return 0;
